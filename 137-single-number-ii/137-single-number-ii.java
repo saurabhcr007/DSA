8b55c6c3b7d684038996f43ddd85798e0462412b
saurabh chuadhary <54533861+saurabhcr007@users.noreply.github.com>
2022-08-16 18:22:04 +0530

    Time: 7 ms (34.85%), Space: 45.5 MB (9.98%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int length=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        int result=0;
+        //int j=0;
+        //ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int i=0;i<length;i++){
+            if(map.get(nums[i])==1){
+                result=nums[i];
+                //result[j]=nums[i];
+                  //  j++;
+            }
+        }
+        return result;
+    }
+}
