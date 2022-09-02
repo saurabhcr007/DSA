8ed7344a1591a37dd387a07cc7639022bb8987bc
saurabh chuadhary <54533861+saurabhcr007@users.noreply.github.com>
2022-09-02 23:40:03 +0530

    Time: 1 ms (67.50%), Space: 40.9 MB (67.73%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.java
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -1,10 +1,5 @@
 class Solution {
     public int sum(int num1, int num2) {
-        for (int i = -200; i <= 200; i ++) {
-            if (num1 + num2 == i) {
-                return i;
-            }
-        }
-	    return -1;
+        return num1+num2;
     }
 }
