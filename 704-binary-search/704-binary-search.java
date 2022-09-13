fdfb2afbf11a1c0350717047b0fa3960e652145e
saurabh chuadhary <54533861+saurabhcr007@users.noreply.github.com>
2022-09-14 00:53:06 +0530

    Time: 0 ms (100.00%), Space: 54.1 MB (66.95%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,44 +1,30 @@
 class Solution {
-    // public int binarysearch(int[] nums,int start,int last,int target){
-    //     if(start<=last){
-    //         int mid=(start+(last-start))/2;
-    //         if(nums[mid]==target){
-    //             return mid;
-    //         }
-    //         if(nums[mid]<target){
-    //             return(binarysearch(nums,mid+1,last,target));
-    //         }
-    //         return(binarysearch(nums,start,mid-1,target));
-    //     }
-    //     return -1;
-    // }
-//     public int binarysearch(int[]a, int first,int last,int target){
-//         if(last>=first)
-//         {
-//             int mid=first+(last-first)/2;
-//             if(a[mid]==target)
-//                 return mid;
-
-//             if(a[mid]>target)
-//                 return(binarysearch(a,first,mid-1,target));
-            
-//             return(binarysearch(a,mid+1,last,target));
-//         }
-//         return -1;
-//     }
-    public int search(int[] nums, int target) {
-        // int start=0;
-        // int last=nums.length-1;
-        // return binarysearch(nums,start,last,target);
-        int n = nums.length;
-        int start = 0;
-        int end = n-1;
-        while(start<=end) {
-            int mid =((start+end)/2);
-            if(nums[mid] == target) return mid;
-            if(nums[mid] < target) start = mid + 1;
-            else end = mid - 1;
+    public int binarysearch(int[] nums,int start,int last,int target){
+        if(start<=last){
+            int mid=(start+(last-start)/2);
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                return(binarysearch(nums,mid+1,last,target));
+            }
+            return(binarysearch(nums,start,mid-1,target));
         }
         return -1;
+    }
+    public int search(int[] nums, int target) {
+        int start=0;
+        int last=nums.length-1;
+        return binarysearch(nums,start,last,target);
+        // int n = nums.length;
+        // int start = 0;
+        // int end = n-1;
+        // while(start<=end) {
+        //     int mid =((start+end)/2);
+        //     if(nums[mid] == target) return mid;
+        //     if(nums[mid] < target) start = mid + 1;
+        //     else end = mid - 1;
+        // }
+        // return -1;
     }
 }
