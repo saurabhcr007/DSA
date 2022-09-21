6c49165f39cd3ebe34b5f9801ae17247f0b083ea
saurabh chuadhary <54533861+saurabhcr007@users.noreply.github.com>
2022-09-21 18:34:57 +0530

    Time: 23 ms (5.69%), Space: 119 MB (78.04%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -3,8 +3,9 @@ class Solution {
         int r=0,c=0;
         int len=original.length;
         int[][] arr=new int[m][n];
+        int[][] temp=new int[0][0];
         if(n*m!=len){
-            return new int[0][0];
+            return temp;
         }
         for(int k=0;k<len;k++){
             arr[r][c]=original[k];
