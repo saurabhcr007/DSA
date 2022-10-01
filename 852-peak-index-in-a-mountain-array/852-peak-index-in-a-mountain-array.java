dc6bb025d1873335ec98cc9d4d522a32fb1195db
saurabh chuadhary <54533861+saurabhcr007@users.noreply.github.com>
2022-10-01 17:43:48 +0530

    Time: 5 ms (5.85%), Space: 73.6 MB (27.40%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -2,6 +2,8 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         for(int i=0;i<arr.length-1;i++)
         {
+            int max=0,temp=0;
+            temp=Math.max(arr[i],arr[i+1]);
             if(arr[i]>arr[i+1])
             {
                 return i;
